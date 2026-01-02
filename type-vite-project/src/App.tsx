7e3dffae9ed9dc1953c64f5afd74e2b4564cbda3
Pankaj Pal <pankajpal8967890080@gmail.comm>
2026-01-02 21:32:50 +0530

    module3

--- type-vite-project/src/App.tsx
+++ type-vite-project/src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <h2>Welcome to React Router Tutorial </h2>
+          <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+            <ul className='navbar-nav mr-auto'>
+              <li><Link to="/" className='nav-link'>Home</Link></li>
+              <li><Link to="/contact" className='nav-link'>Contact</Link></li>
+              <li><Link to="/about" className='nav-link'>About</Link></li>
+            </ul>
+          </nav>
+          <hr />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 }
-
-export default App
+export default App;

473c085f7c386c64de95a04fb1e742f1c82b3bab
Pankaj Pal <pankajpal8967890080@gmail.com>
2026-03-12 11:27:13 +0530

    next

--- frontend class/react-form-validation/src/App.js
+++ frontend class/react-form-validation/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import Form from "./components/Form";
+// import Form from "./components/Form";
+import Form2 from "./components/Form2";
 
 function App() {
   return (
     <div className="container mt-4">
       <h2 className="text-center mb-4">React Form Validation</h2>
-      <Form />
+      {/* <Form /> */}
+      <Form2 />
     </div>
   );
 }

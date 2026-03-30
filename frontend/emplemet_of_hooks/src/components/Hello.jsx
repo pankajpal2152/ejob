e9a6e68e878e0f0ba6f7649b3676f6a3225d480c
Pankaj Pal <pankajpal8967890080@gmail.com>
2026-03-30 15:32:14 +0530

    next

--- frontend/emplemet_of_hooks/src/components/Hello.jsx
+++ frontend/emplemet_of_hooks/src/components/Hello.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+
 const Hello = () => {
 
     // State Declaration
@@ -7,6 +8,23 @@ const Hello = () => {
     // Increase
     const doIncrease = () => {
         setCount(prev => prev + 1);
-    }
+    };
 
-}
+    // Decrease
+    const doDecrease = () => {
+        setCount(prev => (prev > 0 ? prev - 1 : 0));
+    };
+
+    return (
+        <div style={{ textAlign: "center", marginTop: "40px" }}>
+            <h2>Functional Component State(useState Hook)</h2>
+            <h3>Count: {count}</h3>
+            <button onClick={doIncrease} style={{ margin: "10px", padding: "10px" }}>+ Increment </button>
+
+            <button onClick={doDecrease} style={{ margin: "10px", padding: "10px" }}>- Decrement </button>
+        </div>
+    );
+
+};
+
+export default Hello;
